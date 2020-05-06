554db8534b9f8bc5130c89b09b7011fa011d8832
gorre03 <58666442+gorre03@users.noreply.github.com>
2020-05-06 18:05:55 +0200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,7 @@
+<?php
+//IMPOSTAZIONI DA MODIFICARE
+$bitlytoken = 'abcde1234example'; //inserisci qui dentro il token bitly [CAMBPO OBBLIGATORIO]
+$api = '123456789:AAexample'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
+$chats = [12345678, -1000234567]; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
+$ref = 'gorre0307-21'; //inserisci qui dentro il tuo tag amazon.
+$amzn = false; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
