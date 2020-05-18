5e532fb817245e18986f82432a56ee5f73a1b786
sfighello <marconunnari@gmail.com>
2020-05-18 14:13:21 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 //IMPOSTAZIONI DA MODIFICARE
-$bitlytoken = 'abcde1234example'; //inserisci qui dentro il token bitly [CAMBPO OBBLIGATORIO]
-$api = '123456789:AAexample'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
-$chats = [12345678, -1000234567]; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
-$ref = 'gorre0307-21'; //inserisci qui dentro il tuo tag amazon.
+$bitlytoken = 'R_c7d78316d223d5a1d7827d58d80e76be'; //inserisci qui dentro il token bitly [CAMBPO OBBLIGATORIO]
+$api = '1169359396:AAE2uYgsJXm7blM5LMgbnq1hl4QLKsrxvR4'; //inserisci l'api token del tuo bot telegram [CAMBPO OBBLIGATORIO]
+$chats = []; //inserisci qui dentro i chatID che possono usare il bot se vuoi.
+$ref = 'nerdaiolipro-21'; //inserisci qui dentro il tuo tag amazon.
 $amzn = false; //Metti true per referrare anche i link amzn.to (sconsigliato). Consigliato lasciare false.
